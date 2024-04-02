c77a009aadec865378158605cebba728c4237c3a
farish-jamal <farishjamal98@gmail.com>
2024-04-02 14:43:06 +0530

    roaster added

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,50 +1,92 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './LoginPage.css';
-// import DashboardPage from './DashboardPage';
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./LoginPage.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    let loadingToastId;
     try {
-      const response = await fetch("https://zayy-backend.onrender.com/api/auth/admin/login", {
-        method: "POST",
-        headers: { 'Content-Type': "application/json" },
-        body: JSON.stringify({ email: username, password })
+      loadingToastId = toast.info("Logging in. Please wait...", {
+        position: "bottom-right",
+        autoClose: false,
+        hideProgressBar: false,
+        progress: undefined,
+        theme: "light",
       });
+      const response = await fetch(
+        "https://zayy-backend.onrender.com/api/auth/admin/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email: username, password }),
+        }
+      );
       if (response.ok) {
         const data = await response.json();
         localStorage.setItem("token", data.token);
-        navigate("/dashboard");
-      } else
-        console.error("Login failed ");
+        toast.update(loadingToastId, {
+          render: "Login successful! Redirecting...",
+          type: "success",
+          autoClose: 2000,
+        });
+        setTimeout(() => {
+          navigate("/dashboard");
+        }, 2000);
+      }
     } catch (error) {
-      console.log("Error", error)
+      const parsedError = JSON.parse(error.message);
+      console.log("Error message:", parsedError.message);
+      toast.update(loadingToastId, {
+        render: parsedError.message,
+        type: "error",
+        autoClose: 2000,
+      });
     }
   };
 
   return (
-    <div className="background">
-      <div className="container">
-        <h2>Login Page</h2>
-        <form className="form-group" onSubmit={handleLogin}>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <input id="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <button type="submit">Login</button>
-        </form>
+    <>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeButton={false}
+        theme="light"
+      />
+      <div className="background">
+        <div className="container">
+          <h2>Login Page</h2>
+          <form className="form-group" onSubmit={handleLogin}>
+            <div>
+              <label htmlFor="username">Username:</label>
+              <input
+                id="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
